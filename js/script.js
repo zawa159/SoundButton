9c1f8f893cc61ae2b4b068a25ab27331710772ed
zawa159 <shinozawakouta159@gmail.com>
2021-08-01 20:54:36 +0900

    index,css,jsを別ファイルとして記載

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function sound() {
+    // 対象となるID名
+    var id = 'sound' ;
+
+    // 初回以外だったら音声ファイルを巻き戻す
+    if( typeof( document.getElementById( id ).currentTime ) != 'undefined' )
+    {
+        document.getElementById( id ).currentTime = 0;
+    }
+    // [ID:sound-file]の音声ファイルを再生[play()]する
+    document.getElementById( id ).play() ;
+}
